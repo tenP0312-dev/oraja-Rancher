6e73db750982e688018c1207ef72dcfe1b7df623
tenP0312-dev <behkuq54@gmail.com>
2026-08-03 12:51:37 +0900

    Build launcher as a Windows GUI application

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 fn main() {
     if bmsir_arena_launcher::run_self_update_helper_if_requested() {
         return;
